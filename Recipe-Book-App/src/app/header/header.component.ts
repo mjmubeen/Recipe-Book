0912a4912cd66d175fde6b9d60cb62ede3b5ee8a
Mubeen <40416904+mjmubeen@users.noreply.github.com>
2020-10-22 17:58:19 +0500

    Part 8 done
    drop down menu complete

--- Recipe-Book-App/src/app/header/header.component.ts
+++ Recipe-Book-App/src/app/header/header.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styles: ['.open > .dropdown-menu {display:block}']
 })
 export class HeaderComponent implements OnInit {
 
