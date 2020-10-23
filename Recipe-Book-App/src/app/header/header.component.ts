0cac83d2e342b889554368ed8aecdb4df9f39b9c
Mubeen <40416904+mjmubeen@users.noreply.github.com>
2020-10-23 12:08:12 +0500

    Part 8

--- Recipe-Book-App/src/app/header/header.component.ts
+++ Recipe-Book-App/src/app/header/header.component.ts
@@ -4,8 +4,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: ['.open > .dropdown-menu {display:block}']
-})
+  styles: ['.open > .dropdown-menu {display:block}']})
 export class HeaderComponent implements OnInit {
 
   @Output() featureSelcted = new EventEmitter<string>();
